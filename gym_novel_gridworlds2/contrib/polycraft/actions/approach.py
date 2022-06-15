8c380933a709df9ac8d1915b05c7fef66bf8ba15
waymao <my.waymao@gmail.com>
2022-06-15 17:23:25 -0400

    first edition of approach

--- gym_novel_gridworlds2/contrib/polycraft/actions/approach.py
+++ gym_novel_gridworlds2/contrib/polycraft/actions/approach.py
@@ -1,4 +1,96 @@
-from gym_novel_gridworlds2.actions import Action
+from types import NoneType
+from typing import Tuple, Optional, Union
+import queue
+
+from gym_novel_gridworlds2.actions import Action, PreconditionNotMetError
+from gym_novel_gridworlds2.agents import agent
+from gym_novel_gridworlds2.state import State
+from gym_novel_gridworlds2.object.entity import Entity, Object
+import numpy as np
+
+search_directions = {
+    "EAST": np.array((0, 1)),
+    "WEST": np.array((0, -1)),
+    "DOWN": np.array((1, 0)),
+    "UP": np.array((-1, 0))
+}
 
 class Approach(Action):
-    pass
+    def __init__(self, state: State, dynamics=None):
+        self.dynamics = dynamics
+        self.state = state
+
+
+    def _find_nearest_obj(self, 
+            start_loc: tuple, 
+            desired_type: Optional[str]=None, 
+            desired_obj: Optional[Object]=None, 
+            distance: int=1
+        ) -> Optional[Tuple[str, tuple]]:
+        """
+        Takes in the start location, either desired type or desired object, and
+        distance.
+        Does a dfs and finds an object that is closest to the entity.
+        """
+
+        # BFS for a path to the target object
+        visited = np.zeros(self.state._map.shape, dtype=bool)
+        q = queue.Queue()
+        q.put(start_loc)
+
+        while not q.empty():
+            curr_loc = q.get()
+            visited[curr_loc] = True
+            # search if it's at the distance from the agent
+            for direction, vec in search_directions.items():
+                new_loc = tuple(curr_loc + vec * distance)
+                if self.state._map[new_loc] is not None:
+                    if desired_type is not None and self.state._map[new_loc].type == desired_type:
+                        return direction, new_loc
+                    elif desired_obj is not None and self.state._map[new_loc] == desired_obj:
+                        return direction, new_loc
+
+            for _, vec in search_directions.items():
+                new_loc = tuple(np.add(curr_loc, vec))
+                if not visited[new_loc] and self.state._map[new_loc] is None:
+                    q.put(new_loc)
+
+        return (0, 0), None
+
+
+
+    def _check_prepare_action(self,
+        agent_entity: Entity,
+        target_type: str = None,
+        target_object: Object = None,
+        **kwargs
+    ) -> Tuple[bool, str, tuple]:
+        """
+        Helper function for checking if the action is valid or not.
+        """
+        if target_object is None:
+            if target_type is None:
+                raise TypeError("Missing target object instance or type in Approach")
+            target_object_list = self.state.get_objects_of_type(target_type)
+            if len(target_object_list) == 0:
+                return False, None
+            direction, nearest_loc = self._find_nearest_obj(agent_entity.loc, desired_type=target_type)
+            return nearest_loc is None, direction, nearest_loc
+        else:
+            raise NotImplementedError("Approa")
+
+
+    def check_precondition(self, agent_entity: Entity, target_type: str = None, target_object: Object = None, **kwargs):
+        return self._check_prepare_action(agent_entity, target_type, target_object)
+    
+
+    def do_action(self, agent_entity: Entity, target_type: str = None, target_object: Object = None, **kwargs):
+        precondition_met, direction, nearest_loc = self._check_prepare_action(
+            agent_entity, 
+            target_type, 
+            target_object, 
+            distance=kwargs['distance']
+        )
+        if precondition_met:
+            agent_entity.facing = direction
+            self.state.update_object_loc(agent_entity.loc, nearest_loc)
