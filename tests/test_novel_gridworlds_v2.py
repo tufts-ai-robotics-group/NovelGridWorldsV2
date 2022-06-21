5027da12ae8dd45a8debd61959ca478f198b6c12
Arvind Pillai <arvindpillai@Arvinds-MacBook-Pro-2.local>
2022-06-21 16:01:35 -0400

    woops

--- tests/test_novel_gridworlds_v2.py
+++ tests/test_novel_gridworlds_v2.py
@@ -12,13 +12,13 @@ i = 0
 observation, info = env.reset(return_info=True)
 
 for _ in range(1000):
-	for i in n_agents:
-	    observation, reward, done, info = env.step(env.action_space[i].sample())
-	    env.render()
+    for i in n_agents:
+        observation, reward, done, info = env.step(env.action_space[i].sample())
+        env.render()
 
-	    if done:
-	        observation, info = env.reset(return_info=True)
-	        print(observation)
+        if done:
+            observation, info = env.reset(return_info=True)
+            print(observation)
 
 env.close()
 
@@ -35,9 +35,9 @@ env.close()
 #     env.render()
 
 #     if i == n_agents:
-#     	i = 0
+#       i = 0
 #     else:
-#     	i += 1
+#       i += 1
 
 #     if done:
 #         observation, info = env.reset(return_info=True)
