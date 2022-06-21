a90856a493d24c9db536a4cb09f2632accb52051
Arvind Pillai <arvindpillai@Arvinds-MacBook-Pro-2.local>
2022-06-21 16:00:50 -0400

    basic comprehensive test file

---
+++ tests/test_novel_gridworlds_v2.py
@@ -0,0 +1,46 @@
+import gym
+from gym.utils.play import play
+import pygame
+import gym_examples
+
+
+env = gym.make('NovelGridWorlds-v2')
+
+n_agents = 1
+i = 0
+
+observation, info = env.reset(return_info=True)
+
+for _ in range(1000):
+	for i in n_agents:
+	    observation, reward, done, info = env.step(env.action_space[i].sample())
+	    env.render()
+
+	    if done:
+	        observation, info = env.reset(return_info=True)
+	        print(observation)
+
+env.close()
+
+
+# env = gym.make('NovelGridWorlds-v2')
+
+# n_agents = 1
+# i = 0
+
+# observation, info = env.reset(return_info=True)
+
+# for _ in range(1000 * n_agents):
+#     observation, reward, done, info = env.step(env.action_space[i].sample())
+#     env.render()
+
+#     if i == n_agents:
+#     	i = 0
+#     else:
+#     	i += 1
+
+#     if done:
+#         observation, info = env.reset(return_info=True)
+#         print(observation)
+
+# env.close()
