313be990217dee17804d456a3bef3778a099def8
waymao <my.waymao@gmail.com>
2022-06-22 15:07:58 -0400

    fix tabs

--- gym_novel_gridworlds2/utils/MultiAgentActionSpace.py
+++ gym_novel_gridworlds2/utils/MultiAgentActionSpace.py
@@ -12,15 +12,15 @@ class MultiAgentActionSpace(list):
         self._agents_action_space = agents_action_space
 
     def addActionSpace(self, agent_action_space):
-    	"""adds another action space to the list"""
-    	assert isinstance(agent_action_space, gym.spaces.space.Space)
-    	self._agents_action_space.append(agent_action_space)
+        """adds another action space to the list"""
+        assert isinstance(agent_action_space, gym.spaces.space.Space)
+        self._agents_action_space.append(agent_action_space)
 
     def getActionAt(self, index):
-    	return self._agents_action_space[index].sample()
+        return self._agents_action_space[index].sample()
 
     def removeActionSpace(self, index):
-    	self._agents_action_space.pop(index)
+        self._agents_action_space.pop(index)
 
     def sample(self):
         """ samples action for each agent from uniform distribution"""
