84703c964d4c72b81daf4f63c5bc2172fc2c9043
waymao <my.waymao@gmail.com>
2022-06-27 12:27:56 -0400

    non-comlete place and select item

--- gym_novel_gridworlds2/contrib/polycraft/actions/place_item.py
+++ gym_novel_gridworlds2/contrib/polycraft/actions/place_item.py
@@ -1,4 +1,5 @@
 from gym_novel_gridworlds2.actions import Action
+from gym_novel_gridworlds2.object import Entity, Object
 
 class PlaceItem(Action):
     pass
--- gym_novel_gridworlds2/contrib/polycraft/actions/select_item.py
+++ gym_novel_gridworlds2/contrib/polycraft/actions/select_item.py
@@ -1,4 +1,10 @@
 from gym_novel_gridworlds2.actions import Action
+from gym_novel_gridworlds2.object import Entity, Object
 
 class SelectItem(Action):
-    pass
+    def check_precondition(self, agent_entity: Entity, target_type: str = None, target_object: Object = None, **kwargs):
+        return target_type in agent_entity.inventory
+    
+    def do_action(self, agent_entity: Entity, target_type: str = None, target_object: Object = None):
+        if self.check_precondition(agent_entity, target_type, target_object):
+            pass
