a031ba692999b0ce9ab221c724bcfbeb0e717557
Arvind Pillai <arvindpillai@Arvinds-MBP-2.home>
2022-06-27 14:50:56 -0400

    added chest to basic render

--- test_render.py
+++ test_render.py
@@ -9,11 +9,14 @@ from gym_novel_gridworlds2.contrib.polycraft.actions.break_item import Break
 from gym_novel_gridworlds2.contrib.polycraft.actions.craft import Craft
 from gym_novel_gridworlds2.contrib.polycraft.actions.use import Use
 from gym_novel_gridworlds2.object.entity import Entity
-from gym_novel_gridworlds2.contrib.polycraft.objects.polycraft_obj import PolycraftObject
+from gym_novel_gridworlds2.contrib.polycraft.objects.polycraft_obj import (
+    PolycraftObject,
+)
 from gym_novel_gridworlds2.contrib.polycraft.objects.door import Door
 from gym_novel_gridworlds2.contrib.polycraft.objects.chest import Chest
 
-class TestRender():
+
+class TestRender:
     def setUp(self):
 
         """
@@ -26,54 +29,44 @@ class TestRender():
         self.state.random_place("rubber", 1, PolycraftObject)
         """
 
-        #MEDIUM
+        # MEDIUM
         self.state = State(map_size=(15, 15), objects=[])
         self.state.init_border()
-        pogoist: Entity = self.state.place_object("agent", Entity, properties={"loc": (2, 2)})
+        pogoist: Entity = self.state.place_object(
+            "agent", Entity, properties={"loc": (2, 2)}
+        )
         pogoist.inventory = {}
-        
 
         for i in range(6):
             if i == 0:
                 continue
-            self.state.place_object("bedrock", PolycraftObject, properties={"loc": (i, 9)})
+            self.state.place_object(
+                "bedrock", PolycraftObject, properties={"loc": (i, 9)}
+            )
         for i in range(4):
-            self.state.place_object("bedrock", PolycraftObject, properties={"loc": (5, 10 + i)})
+            self.state.place_object(
+                "bedrock", PolycraftObject, properties={"loc": (5, 10 + i)}
+            )
         self.state.remove_object("bedrock", (4, 9))
         self.state.place_object("door", Door, properties={"loc": (4, 9)})
+        self.state.place_object("chest", Chest, properties={"loc": (2, 13)})
+
+        cObj = self.state.get_object_at((2, 13))
+        cObj.inventory = {"iron": 3}
 
         self.state.random_place("tree", 3, PolycraftObject)
         self.state.random_place("rubber", 1, PolycraftObject)
 
         recipe_dict = {
-        "recipes": {
-            "stick": {
-              "input": [
-                {"plank": 2}
-              ],
-              "output": [
-                {"stick": 4}
-              ]
-            },
-            "plank": {
-              "input": [
-                {"tree": 1}
-              ],
-              "output": [
-                {"plank": 4}
-              ]
-            },
-            "pogo_stick": {
-              "input": [
-                {"stick": 4},
-                {"plank": 2},
-                {"rubber": 1}
-              ],
-              "output": [
-                {"pogo_stick": 1}
-              ]
+            "recipes": {
+                "stick": {"input": [{"plank": 2}], "output": [{"stick": 4}]},
+                "plank": {"input": [{"tree": 1}], "output": [{"plank": 4}]},
+                "pogo_stick": {
+                    "input": [{"stick": 4}, {"plank": 2}, {"rubber": 1}],
+                    "output": [{"pogo_stick": 1}],
+                },
             }
-        }}
+        }
 
         self.actions = {
             "up": Move(direction="UP", state=self.state),
@@ -82,59 +75,75 @@ class TestRender():
             "right": Move(direction="RIGHT", state=self.state),
             "break": Break(state=self.state),
             "use": Use(state=self.state),
-            "craft_stick": Craft(state=self.state, recipe=recipe_dict["recipes"]["stick"]),
-            "craft_plank": Craft(state=self.state, recipe=recipe_dict["recipes"]["plank"]),
-            "craft_pogo_stick": Craft(state=self.state, recipe=recipe_dict["recipes"]["pogo_stick"])
+            "craft_stick": Craft(
+                state=self.state, recipe=recipe_dict["recipes"]["stick"]
+            ),
+            "craft_plank": Craft(
+                state=self.state, recipe=recipe_dict["recipes"]["plank"]
+            ),
+            "craft_pogo_stick": Craft(
+                state=self.state, recipe=recipe_dict["recipes"]["pogo_stick"]
+            ),
         }
-        
 
     def getSymbol(self, obj, state, canWalkOver=False, facing="NORTH"):
         if obj == "tree":
             if state == "block":
-                return 'T'
+                return "T"
             else:
-                return 't'
+                return "t"
         elif obj == "air":
-            return ' '
+            return " "
         elif obj == "bedrock":
-            return 'X'
+            return "X"
         elif obj == "door":
             if canWalkOver == True:
-                return ' '
+                return " "
             else:
-                return 'D'
+                return "D"
         elif obj == "rubber":
             if state == "block":
-                return 'R'
+                return "R"
             else:
-                return 'r'
+                return "r"
+        elif obj == "chest":
+            if state == "block":
+                return "C"
+            else:
+                return "c"
         elif obj == "agent":
             if facing == "NORTH":
-                return '^'
+                return "^"
             elif facing == "SOUTH":
-                return 'v'
+                return "v"
             elif facing == "EAST":
                 return ">"
             else:
                 return "<"
         else:
-            return ' '
+            return " "
 
     def mapRepresentation(self):
         res: np.ndarray = np.empty(self.state.initial_info["map_size"], dtype="object")
         for i in range(self.state.initial_info["map_size"][0]):
             for j in range(self.state.initial_info["map_size"][1]):
-                obj = self.state.get_objects_at((i,j))
+                obj = self.state.get_objects_at((i, j))
                 if len(obj[0]) != 0:
                     if hasattr(obj[0][0], "canWalkOver"):
-                        res[i][j] = self.getSymbol(obj[0][0].type, obj[0][0].state, canWalkOver=obj[0][0].canWalkOver)
+                        res[i][j] = self.getSymbol(
+                            obj[0][0].type,
+                            obj[0][0].state,
+                            canWalkOver=obj[0][0].canWalkOver,
+                        )
                     else:
                         res[i][j] = self.getSymbol(obj[0][0].type, obj[0][0].state)
                 else:
-                    res[i][j] = ' '
+                    res[i][j] = " "
                 if len(obj[1]) != 0:
                     if hasattr(obj[1][0], "facing"):
-                        res[i][j] = self.getSymbol(obj[1][0].type, obj[1][0].state, facing=obj[1][0].facing)
+                        res[i][j] = self.getSymbol(
+                            obj[1][0].type, obj[1][0].state, facing=obj[1][0].facing
+                        )
                     else:
                         res[i][j] = self.getSymbol(obj[1][0].type, obj[1][0].state)
         print(res)
@@ -152,7 +161,9 @@ class TestRender():
                 print("Agent's Inventory:")
                 print(agent.inventory)
                 print("")
-                print("Actions: up, down, left, right, break, craft_stick, craft_plank, craft_pogo_stick")
+                print(
+                    "Actions: up, down, left, right, break, use, craft_stick, craft_plank, craft_pogo_stick"
+                )
                 choice = input("Select an action: ")
                 if choice == "up":
                     self.actions["up"].do_action(agent)
@@ -173,6 +184,7 @@ class TestRender():
                 elif choice == "craft_pogo_stick":
                     self.actions["craft_pogo_stick"].do_action(agent)
 
+
 def main():
     print("Goal: Craft Pogostick (1 Rubber, 2 Planks, 4 Sticks)")
     time.sleep(1)
@@ -180,5 +192,6 @@ def main():
     test.setUp()
     test.mainLoop()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
