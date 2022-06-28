867bc1e2c5def1297cc0a1f0af8f2e2d914f8cfb
waymao <my.waymao@gmail.com>
2022-06-28 16:15:34 -0400

    test parse objects done and all passed

--- gym_novel_gridworlds2/contrib/polycraft/objects/__init__.py
+++ gym_novel_gridworlds2/contrib/polycraft/objects/__init__.py
@@ -1,2 +1,4 @@
 from .polycraft_entity import PolycraftEntity
 from .polycraft_obj import PolycraftObject
+from .door import Door
+from .chest import Chest
--- tests/polycraft/parser/test_parser.py
+++ tests/polycraft/parser/test_parser.py
@@ -1,4 +1,6 @@
 import unittest
+from gym_novel_gridworlds2.contrib.polycraft.objects.door import Door
+from gym_novel_gridworlds2.contrib.polycraft.objects.polycraft_obj import PolycraftObject
 from gym_novel_gridworlds2.utils.json_parser import ConfigParser
 import gym
 from gym.utils.play import play
@@ -42,10 +44,19 @@ class ActionSpaceTests(unittest.TestCase):
         self.assertTrue("move_right" in dynamic.actions)
         self.assertEqual(dynamic.actions['move_right'].vec, (0, 1))
     
+    
     def test_objects(self):
         file_name = "parser_test_objects.json"
 
         config_file_path = self.get_file_path(file_name)
         state, dynamic, entities = self.json_parser.parse_json(config_file_path)
+
+        obj_door = state.get_objects_at((2, 3))[0][0]
+        self.assertEqual(obj_door.type, "door")
+        self.assertEqual(obj_door.__class__, Door)
+
+        obj_tree = state.get_objects_at((3, 4))[0][0]
+        self.assertEqual(obj_tree.type, "tree")
+        self.assertEqual(obj_tree.__class__, PolycraftObject)
 
 
