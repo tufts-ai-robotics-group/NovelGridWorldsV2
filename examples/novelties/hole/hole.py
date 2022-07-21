cdbf145ae80e2e68da3457c11852fe9e1d66971b
clarech712 <clarech712@gmail.com>
2022-07-21 15:42:43 -0400

    agent loses everything in inventory on stepping on hole

---
+++ examples/novelties/hole/hole.py
@@ -0,0 +1,25 @@
+from gym_novel_gridworlds2.contrib.polycraft.objects.polycraft_obj import PolycraftObject
+
+
+class Hole(PolycraftObject):
+    def __init__(self, typee, loc=(0, 0), state="block", inventory=None, **kwargs):
+        if inventory is None:
+            inventory = {}
+        self.type = typee
+        self.loc = loc  # update such that we update the 3D arr and add the item to it
+        self.state = state  # two states: block and floating
+        self.canWalkOver = True
+        self.inventory = inventory
+
+    def placement_reqs(self, map_state, loc):
+        return True
+
+    def acted_upon(self, action_name, agent):
+        """if action_name == "use":
+            if self.canWalkOver == True:
+                self.canWalkOver = False
+            else:
+                self.canWalkOver = True"""
+        if action_name == "break":
+            agent.inventory.update(self.inventory)
+            self.state = "floating"
