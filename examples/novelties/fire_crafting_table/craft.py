502bb63bd7da324c61e4c2b06fcc68b8d5c791d3
clarech712 <clarech712@gmail.com>
2022-07-21 14:41:16 -0400

    crafting table on fire must collect water to put fire out

---
+++ examples/novelties/fire_crafting_table/craft.py
@@ -0,0 +1,79 @@
+from gym_novel_gridworlds2.state import State
+from gym_novel_gridworlds2.actions import Action, PreconditionNotMetError
+from gym_novel_gridworlds2.object.entity import Entity, Object
+from gym_novel_gridworlds2.utils.recipe import Recipe
+
+import numpy as np
+
+
+class Craft(Action):
+    def __init__(self, state: State, recipe, dynamics=None):
+        self.state = state
+        self.recipe = recipe
+        self.itemToCraft = list(recipe["output"][0].keys())[0]
+
+    def check_precondition(
+        self, agent_entity: Entity, target_type=None, target_object=None
+    ):
+        """
+        Checks preconditions of the craft action:
+        1) The agent must have all of the necessary inputs
+        2) The agent must be adjacent to a crafting table if the recipe needs a crafting table
+        """
+        count = 0
+        for item in self.recipe["input"]:
+            temp = list(item.keys())
+            if temp[0] in agent_entity.inventory:
+                if (
+                    self.recipe["input"][count][temp[0]]
+                    > agent_entity.inventory[temp[0]]
+                ):
+                    return False  # not enough of the item
+            else:
+                return False  # one of the inputs isnt in the agents inventory
+            count += 1
+        if "needs_table" not in self.recipe or self.recipe["needs_table"] == "False":
+            return True
+        else:
+            # convert the entity facing direction to coords
+            direction = (0, 0)
+            if agent_entity.facing == "NORTH":
+                direction = (-1, 0)
+            elif agent_entity.facing == "SOUTH":
+                direction = (1, 0)
+            elif agent_entity.facing == "EAST":
+                direction = (0, 1)
+            else:
+                direction = (0, -1)
+
+            correctDirection = False
+
+            self.temp_loc = tuple(np.add(agent_entity.loc, direction))
+            objs = self.state.get_objects_at(self.temp_loc)
+            if len(objs[0]) == 1:
+                if objs[0][0].type == "crafting_table" and objs[0][0].fire_state == "available" and objs[0][0].state == "block":
+                    return True
+                else:
+                    return False
+
+    def do_action(self, agent_entity: Entity, target_type=None, target_object=None):
+        if not self.check_precondition(agent_entity):
+            raise PreconditionNotMetError(
+                f"Agent {agent_entity.name} cannot craft {self.itemToCraft}."
+            )
+
+        count = 0
+        for item in self.recipe["input"]:
+            temp = list(item.keys())
+            agent_entity.inventory[temp[0]] = (
+                agent_entity.inventory[temp[0]] - self.recipe["input"][count][temp[0]]
+            )
+            count += 1
+        if self.itemToCraft in agent_entity.inventory:
+            agent_entity.inventory[self.itemToCraft] += self.recipe["output"][0][
+                self.itemToCraft
+            ]
+        else:
+            agent_entity.inventory[self.itemToCraft] = self.recipe["output"][0][
+                self.itemToCraft
+            ]
---
+++ examples/novelties/fire_crafting_table/table.py
@@ -0,0 +1,20 @@
+from gym_novel_gridworlds2.contrib.polycraft.objects.polycraft_obj import PolycraftObject
+
+
+class CraftingTable(PolycraftObject):
+    def __init__(self, typee, loc=(0, 0), state="block", fire_state="on_fire", **kwargs):
+        self.type = typee
+        self.loc = loc  # update such that we update the 3D arr and add the item to it
+        self.state = state  # two states: on_fire and available
+        self.fire_state = fire_state
+        self.canWalkOver = False
+
+    def placement_reqs(self, map_state, loc):
+        return True
+
+    def acted_upon(self, action_name, agent):
+        if action_name == "spray_water" and agent.inventory["water"] > 0:
+            self.fire_state = "available"
+            agent.inventory["water"] -= 1
+        if action_name == "break":
+            self.state = "floating"
