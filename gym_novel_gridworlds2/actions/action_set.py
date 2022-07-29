08136dbabc08e574e983d62cb5c8d28f6d324d89
waymao <my.waymao@gmail.com>
2022-07-29 10:54:01 -0400

    allow flexible command

--- gym_novel_gridworlds2/actions/action_set.py
+++ gym_novel_gridworlds2/actions/action_set.py
@@ -1,15 +1,45 @@
 from typing import List, Tuple
 from ..object.entity import Entity
 from .action import Action 
+import re
 from gym.spaces import Discrete
 
 class ActionSet:
     def __init__(self, actions: List[Tuple[str, Action]]):
         self.actions = actions
+
+        # symbolic functionalities: used to parse commands
+        self.action_index = {}
+        for i, (name, _) in enumerate(actions):
+            self.action_index[name] = i
     
     def do_action(self, entity, index):
-        action: Action = self.actions[index]
+        action = self.actions[index][1]
         action.do_action(entity)
+    
+    def parse_exec_command(self, command, entity):
+        """
+        Symbolic functionalities:
+        Parse and execute a command
+        """
+        cmd_name = command.split(" ")[0]
+        if cmd_name not in self.action_index:
+            raise KeyError(f"Command \"{command}\" not found")
+        
+        # get action
+        action_id = self.action_index[cmd_name]
+        _, action = self.actions[action_id]
+
+        # try to parse parameters
+        if hasattr(action, "cmd_format") and action.cmd_format is not None:
+            match = re.match(self.action_index[cmd_name].cmd_format, command)
+            if match is not None:
+                # found matching parameters, do the action using the parameters
+                params = match.groupdict()
+                return action.do_action(entity, **params)
+        
+        # fallback: no cmd_format specified and or input format not matched
+        return action.do_action(entity)
 
     def remove_action(self, index):
         pass
