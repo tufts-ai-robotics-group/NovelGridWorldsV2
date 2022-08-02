1acb9dd6a83176929dcefc1bbc7a850c77420b57
Sulfruos <arvupillai72@gmail.com>
2022-08-02 16:58:39 -0400

    partially working pogoist

--- gym_novel_gridworlds2/agents/pogoist.py
+++ gym_novel_gridworlds2/agents/pogoist.py
@@ -6,77 +6,171 @@ class Pogoist(Agent):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.isMoving = False
-        # self.policy_step = 0
-        # self.tree_count = 0
+        self.policy_step = 0
+        self.rotate_step = 0
 
     def policy(self, observation):
-        if self.isMoving:
-            self.isMoving = False
-            action_sets = self.action_set.get_action_names()
-            to_do = self.get_action_space().sample()
-            while to_do == action_sets.index("NOP"):
-                to_do = self.get_action_space().sample()
-            return to_do
-        else:  # skip every other turn
-            self.isMoving = True
-            action_sets = self.action_set.get_action_names()
-            return action_sets.index("NOP")
+
+        ent = self.state.get_entity_by_id(102)
+        print("facing" + ent.facing)
+        print(ent.loc)
+
+        # print("state step count: ", self.state._step_count)
+        # if self.isMoving:
+        #     self.isMoving = False
+        #     action_sets = self.action_set.get_action_names()
+        #     to_do = self.get_action_space().sample()
+        #     while to_do == action_sets.index("NOP"):
+        #         to_do = self.get_action_space().sample()
+        #     return to_do
+        # else:  # skip every other turn
+        #     self.isMoving = True
+        #     action_sets = self.action_set.get_action_names()
+        #     return action_sets.index("NOP")
 
         # ent = self.state.get_entity_by_id(102)
         # print("facing" + ent.facing)
         # print(ent.loc)
 
-        # if self.isMoving:
-        #     self.isMoving = False
-        #     action_sets = self.action_set.get_action_names()
-        #     if self.policy_step == 0 or self.policy_step == 5 or self.policy_step == 8:
-        #         objs = self.state.get_objects_of_type("oak_log")
-        #         self.tree_count += 1
-        #         if len(objs) > 0:
-        #             self.policy_step += 1
-        #             return action_sets.index(
-        #                 "TP_TO_"
-        #                 + str(objs[self.tree_count - 1].loc[0])
-        #                 + ",17,"
-        #                 + str(objs[self.tree_count - 1].loc[1])
-        #             )
-        #     elif self.policy_step == 1:
-        #         vec = (0, 0)
-        #         if self.entity_data.facing == "NORTH":
-        #             vec = (-1, 0)
-        #         elif self.entity_data.facing == "SOUTH":
-        #             vec = (1, 0)
-        #         elif self.entity_data.facing == "WEST":
-        #             vec = (0, -1)
-        #         else:
-        #             vec = (0, 1)
+        if self.isMoving:
+            if self.rotate_step == 4:
+                self.policy_step -= 1  # tree obviously gone, must find new one
+                self.rotate_step = 0
+            self.isMoving = False
+            action_sets = self.action_set.get_action_names()
+            if (
+                self.policy_step == 0
+                or self.policy_step == 4
+                or self.policy_step == 8
+                or self.policy_step == 19
+            ):
+                objs = self.state.get_objects_of_type("oak_log")
+                if len(objs) > 0:
+                    self.policy_step += 1
+                    return action_sets.index(
+                        "TP_TO_" + str(objs[0].loc[0]) + ",17," + str(objs[0].loc[1])
+                    )
+            elif (
+                self.policy_step == 1
+                or self.policy_step == 5
+                or self.policy_step == 9
+                or self.policy_step == 20
+            ):
+                vec = (0, 0)
+                if ent.facing == "NORTH":
+                    vec = (-1, 0)
+                elif ent.facing == "SOUTH":
+                    vec = (1, 0)
+                elif ent.facing == "WEST":
+                    vec = (0, -1)
+                else:
+                    vec = (0, 1)
 
-        #         new_loc = np.add(vec, self.entity_data.loc)
-        #         print(new_loc)
-        #         objs = self.state.get_objects_at(tuple(new_loc))
-        #         print(objs)
-        #         if len(objs[0]) > 0:
-        #             print(objs[0][0].type)
-        #             if objs[0][0].type != "oak_log":
-        #                 return action_sets.index("rotate_right")
-        #                 # need to rotate until we are facing the oak log
-        #             else:
-        #                 self.policy_step += 1
-        #                 return action_sets.index("NOP")
-        #                 # do nothing as already facing the log, move onto next part of policy
-        #         else:
-        #             return action_sets.index("rotate_right")
-        #             # need to rotate until we are facing the oak log
-        #     elif self.policy_step == 2:
-        #         self.policy_step += 1
-        #         return action_sets.index("break")
-        #     elif self.policy_step == 3:
-        #         self.policy_step += 1
-        #         return action_sets.index("forward")
-        # else:  # skip every other turn
-        #     self.isMoving = True
-        #     action_sets = self.action_set.get_action_names()
-        #     return action_sets.index("NOP")
+                new_loc = np.add(vec, ent.loc)
+                objs = self.state.get_objects_at(tuple(new_loc))
+                if len(objs[0]) > 0:
+                    if objs[0][0].type != "oak_log":
+                        self.rotate_step += 1
+                        return action_sets.index("rotate_right")
+                        # need to rotate until we are facing the oak log
+                    else:
+                        self.policy_step += 1
+                        self.rotate_step = 0
+                        return action_sets.index("NOP")
+                        # do nothing as already facing the log, move onto next part of policy
+                else:
+                    self.rotate_step += 1
+                    return action_sets.index("rotate_right")
+                    # need to rotate until we are facing the oak log
+            elif (
+                self.policy_step == 2 or self.policy_step == 6 or self.policy_step == 10
+            ):
+                self.policy_step += 1
+                return action_sets.index("break")
+            elif (
+                self.policy_step == 3 or self.policy_step == 7 or self.policy_step == 11
+            ):
+                self.policy_step += 1
+                return action_sets.index("forward")
+            elif self.policy_step == 12:
+                objs = self.state.get_objects_of_type("crafting_table")
+                if len(objs) > 0:
+                    self.policy_step += 1
+                    return action_sets.index(
+                        "TP_TO_" + str(objs[0].loc[0]) + ",17," + str(objs[0].loc[1])
+                    )
+            elif self.policy_step == 13:
+                vec = (0, 0)
+                if ent.facing == "NORTH":
+                    vec = (-1, 0)
+                elif ent.facing == "SOUTH":
+                    vec = (1, 0)
+                elif ent.facing == "WEST":
+                    vec = (0, -1)
+                else:
+                    vec = (0, 1)
+
+                new_loc = np.add(vec, ent.loc)
+                objs = self.state.get_objects_at(tuple(new_loc))
+                if len(objs[0]) > 0:
+                    if objs[0][0].type != "crafting_table":
+                        self.rotate_step += 1
+                        return action_sets.index("rotate_right")
+                        # need to rotate until we are facing the crafting table
+                    else:
+                        self.policy_step += 1
+                        self.rotate_step = 0
+                        return action_sets.index("NOP")
+                        # do nothing as already facing the log, move onto next part of policy
+                else:
+                    self.rotate_step += 1
+                    return action_sets.index("rotate_right")
+                    # need to rotate until we are facing the crafting table
+            elif (
+                self.policy_step == 14
+                or self.policy_step == 15
+                or self.policy_step == 16
+            ):
+                self.policy_step += 1
+                return action_sets.index("craft_plank")
+            elif self.policy_step == 17:
+                self.policy_step += 1
+                return action_sets.index("craft_stick")
+            elif self.policy_step == 18:
+                self.policy_step += 1
+                return action_sets.index("craft_tree_tap")
+            elif self.policy_step == 21:
+                vec = (0, 0)  # inverse direction so we go backwards
+                if ent.facing == "NORTH":
+                    vec = (1, 0)
+                elif ent.facing == "SOUTH":
+                    vec = (-1, 0)
+                elif ent.facing == "WEST":
+                    vec = (0, 1)
+                else:
+                    vec = (0, -1)
+
+                new_loc = np.add(vec, ent.loc)
+                print(ent.loc, new_loc)
+                objs = self.state.get_objects_at(tuple(new_loc))
+                if len(objs[0]) == 0:
+                    self.policy_step += 1
+                    return action_sets.index(
+                        "TP_TO_" + str(new_loc[0]) + ",17," + str(new_loc[1])
+                    )
+                else:
+                    return action_sets.index("NOP")
+            elif self.policy_step == 22:
+                self.policy_step += 1
+                return action_sets.index("select_tree_tap")
+            elif self.policy_step == 23:
+                self.policy_step += 1
+                return action_sets.index("place_item")
+
+        else:  # skip every other turn
+            self.isMoving = True
+            action_sets = self.action_set.get_action_names()
+            return action_sets.index("NOP")
         # find tree, teleport to said tree
         # break tree
         # move forward to get log
