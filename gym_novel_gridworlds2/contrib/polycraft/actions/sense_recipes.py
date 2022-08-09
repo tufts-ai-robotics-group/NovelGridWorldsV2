c10236fde24ff8a9f29f17e397bf57fd7fb083fe
Sulfruos <arvupillai72@gmail.com>
2022-08-09 13:29:06 -0400

    also fixed recipes

--- gym_novel_gridworlds2/contrib/polycraft/actions/sense_recipes.py
+++ gym_novel_gridworlds2/contrib/polycraft/actions/sense_recipes.py
@@ -1,5 +1,6 @@
 from xmlrpc.client import Boolean
 from gym_novel_gridworlds2.state import State
+from gym_novel_gridworlds2.utils import nameConversion
 from gym_novel_gridworlds2.actions import Action, PreconditionNotMetError
 from gym_novel_gridworlds2.object.entity import Entity, Object
 
@@ -34,20 +35,18 @@ class SenseRecipes(Action):
         recipe_set: RecipeSet = self.dynamics.recipe_set
         output = []
         for recipe in recipe_set.recipes.values():
-            output.append({
-                "inputs": [
-                    {
-                        "Item": item,
-                        "stackSize": 1,
-                        "slot": i
-                    } for i, item in enumerate(recipe.input_list) if item is not None and item != "0"
-                ],
-                "outputs": [
-                    {
-                        "Item": item,
-                        "stackSize": 1,
-                        "slot": i
-                    } for i, item in enumerate(recipe.output_list) if item is not None and item != "0"
-                ]
-            })
+            output.append(
+                {
+                    "inputs": [
+                        {"Item": nameConversion(item), "stackSize": 1, "slot": i}
+                        for i, item in enumerate(recipe.input_list)
+                        if item is not None and item != "0"
+                    ],
+                    "outputs": [
+                        {"Item": nameConversion(item), "stackSize": 1, "slot": i}
+                        for i, item in enumerate(recipe.output_list)
+                        if item is not None and item != "0"
+                    ],
+                }
+            )
         return {"recipes": output}
