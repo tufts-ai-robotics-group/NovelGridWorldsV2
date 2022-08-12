7221d31943d9b93559c875deec7a1ced27fdca48
Sulfruos <arvupillai72@gmail.com>
2022-08-12 15:08:13 -0400

    metal

--- gym_novel_gridworlds2/contrib/polycraft/objects/__init__.py
+++ gym_novel_gridworlds2/contrib/polycraft/objects/__init__.py
@@ -3,6 +3,7 @@ from .polycraft_obj import PolycraftObject
 from .door import Door
 from .chest import Chest
 from .diamond_ore import DiamondOre
+from .metal import Metal
 from .block_of_platinum import BlockOfPlatinum
 from .iron_pickaxe import IronPickaxe
 from .tree_tap import TreeTap
@@ -23,5 +24,5 @@ __all__ = [
     "PlasticChest",
     "Safe",
     "EntityPogoist",
-    "EntityTrader"
+    "EntityTrader",
 ]
