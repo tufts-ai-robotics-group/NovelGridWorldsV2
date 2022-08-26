34470275c540269e0d0282c99e3763913b0162ac
waymao <my.waymao@gmail.com>
2022-08-26 14:28:35 -0400

    fixed collection from safe

--- gym_novel_gridworlds2/contrib/polycraft/objects/safe.py
+++ gym_novel_gridworlds2/contrib/polycraft/objects/safe.py
@@ -1,6 +1,17 @@
 from .polycraft_obj import PolycraftObject
 
 
+def merge_inventory(old_item, new_item):
+    """
+    Merges two items into one item.
+    """
+    for key, value in new_item.items():
+        if key in old_item:
+            old_item[key] += value
+        else:
+            old_item[key] = value
+
+
 class Safe(PolycraftObject):
     def __init__(self, typee, loc=(0, 0), state="block", inventory=None, **kwargs):
         if inventory is None:
@@ -17,10 +28,10 @@ class Safe(PolycraftObject):
     def acted_upon(self, action_name, agent):
         if action_name == "break":
             pass  # unbreakable
-        if action_name == "use":
+        elif action_name == "use":
             if "blue_key" in agent.inventory:
                 self.isLocked = False
                 self.type == "unlocked_safe"
-        if action_name == "collect":
-            agent.inventory.update(self.inventory)
+        elif action_name == "collect" and not self.isLocked:
+            merge_inventory(agent.inventory, self.inventory)
             self.inventory = {}
