c66ed193e284cf4ad403ae34f7467d93afbaface
Sulfruos <arvupillai72@gmail.com>
2022-11-18 17:02:02 -0500

    more place item tests

--- tests/polycraft/test_place_item.py
+++ tests/polycraft/test_place_item.py
@@ -3,7 +3,9 @@ import unittest
 from gym_novel_gridworlds2.actions.action import PreconditionNotMetError
 
 from gym_novel_gridworlds2.state import State
-from gym_novel_gridworlds2.contrib.polycraft.actions.smoothmove import SmoothMove as Move
+from gym_novel_gridworlds2.contrib.polycraft.actions.smoothmove import (
+    SmoothMove as Move,
+)
 from gym_novel_gridworlds2.contrib.polycraft.actions.break_item import Break
 from gym_novel_gridworlds2.contrib.polycraft.actions.use import Use
 from gym_novel_gridworlds2.contrib.polycraft.actions.select_item import SelectItem
@@ -18,9 +20,10 @@ from gym_novel_gridworlds2.state.dynamic import Dynamic
 
 class PlaceItemTests(unittest.TestCase):
     def setUp(self):
-        self.state = State(map_size=(5, 8), objects=[])
+        self.state = State(map_size=(10, 10), objects=[])
         self.obj_types = {
             "chest": {"module": Chest, "params": {}},
+            "crafting_table": {"module": PolycraftObject, "params": {}},
             "door": {"module": Door, "params": {}},
             "default": {"module": PolycraftObject, "params": {}},
             "tree_tap": {"module": TreeTap, "params": {}},
@@ -29,10 +32,10 @@ class PlaceItemTests(unittest.TestCase):
         self.dynamics = Dynamic(None, None, None, self.obj_types, None)
 
         self.actions = {
-            "up": Move(direction="UP", state=self.state),
-            "down": Move(direction="DOWN", state=self.state),
-            "left": Move(direction="LEFT", state=self.state),
-            "right": Move(direction="RIGHT", state=self.state),
+            "up": Move(direction="W", state=self.state),
+            "down": Move(direction="X", state=self.state),
+            "left": Move(direction="A", state=self.state),
+            "right": Move(direction="D", state=self.state),
             "break": Break(state=self.state),
             "use": Use(state=self.state),
             "select_item": SelectItem(state=self.state),
@@ -102,7 +105,7 @@ class PlaceItemTests(unittest.TestCase):
         self.assertEqual(agent.inventory[agent.selectedItem], 1)
 
         self.state.clear()
-    
+
     def testPlaceTreeTap(self):
         agent = self.state.place_object("agent", Entity, properties={"loc": (1, 2)})
         agent.inventory = {"oak_log": 2, "tree_tap": 1}
@@ -132,7 +135,6 @@ class PlaceItemTests(unittest.TestCase):
         self.assertEqual(hbn.type, "tree_tap")
         self.assertEqual(agent.inventory["tree_tap"], 0)
 
-
     def testPlaceDoor(self):
         agent = self.state.place_object("agent", Entity, properties={"loc": (1, 2)})
         agent.inventory = {"door": 1}
@@ -153,3 +155,100 @@ class PlaceItemTests(unittest.TestCase):
         self.assertEqual(agent.inventory[agent.selectedItem], 0)
 
         self.state.clear()
+
+    def testPlaceCraftingTable(self):
+        ctable = self.state.place_object(
+            "crafting_table", PolycraftObject, properties={"loc": (0, 2)}
+        )
+        agent = self.state.place_object(
+            "agent", Entity, properties={"loc": (ctable.loc[0] + 1, ctable.loc[1])},
+        )
+
+        print("AGENT: ", agent.loc)
+
+        hbn = self.state.get_object_at(ctable.loc)
+        self.assertEqual(hbn.type, "crafting_table")
+
+        self.actions["break"].do_action(agent, hbn)
+        self.actions["up"].do_action(agent)
+
+        ctable2 = self.state.get_objects_of_type("crafting_table")
+        self.assertEqual(ctable2, [])
+
+        print("LOC1: ", ctable.loc)
+
+        print("AGENT: ", agent.loc)
+        print(agent.inventory)
+
+        self.actions["select_item"].do_action(agent, "crafting_table")
+
+        self.actions["down"].do_action(agent)
+        self.actions["down"].do_action(agent)
+        self.actions["down"].do_action(agent)
+        self.actions["up"].do_action(agent)
+
+        self.assertEqual(agent.selectedItem, "crafting_table")
+
+        print("AGENT: ", agent.loc)
+
+        self.actions["place_item"].do_action(agent, "crafting_table")
+
+        print(agent.inventory)
+
+        print("LOC2: ", ctable.loc)
+
+        hbn = self.state.get_object_at(ctable.loc)
+        self.assertEqual(hbn, None)
+
+        self.state.clear()
+
+    def testPlaceCraftingTableUsingParser(self):
+        self.state.random_place_in_room(
+            "crafting_table", 1, [0, 0], [5, 8], PolycraftObject, properties=dict()
+        )
+        ctable = self.state.get_objects_of_type("crafting_table")[0]
+        print("LOC3: ", ctable.loc)
+
+        agent = self.state.place_object(
+            "agent", Entity, properties={"loc": (ctable.loc[0] + 1, ctable.loc[1])},
+        )
+
+        print("AGENT: ", agent.loc)
+
+        hbn = self.state.get_object_at(ctable.loc)
+        self.assertEqual(hbn.type, "crafting_table")
+
+        self.actions["break"].do_action(agent, hbn)
+
+        self.actions["up"].do_action(agent)
+
+        ctable2 = self.state.get_objects_of_type("crafting_table")
+        self.assertEqual(ctable2, [])
+
+        print("LOC2: ", ctable.loc)
+
+        print(agent.loc)
+        print(agent.inventory)
+
+        self.actions["select_item"].do_action(agent, "crafting_table")
+
+        self.actions["down"].do_action(agent)
+        self.actions["down"].do_action(agent)
+        self.actions["down"].do_action(agent)
+        self.actions["up"].do_action(agent)
+
+        self.assertEqual(agent.selectedItem, "crafting_table")
+
+        print(agent.loc)
+
+        self.actions["place_item"].do_action(agent, "crafting_table")
+
+        print(agent.inventory)
+
+        print("LOC3: ", ctable.loc)
+
+        hbn = self.state.get_object_at(ctable.loc)
+        self.assertEqual(hbn, None)
+
+        self.state.clear()
+
