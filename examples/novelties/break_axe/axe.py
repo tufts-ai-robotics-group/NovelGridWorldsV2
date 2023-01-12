9850e19c7a3963cb1ab30dada97da01fd35b6422
waymao <my.waymao@gmail.com>
2023-01-12 18:23:13 -0500

    break_axe hard config fixed

--- examples/novelties/break_axe/axe.py
+++ examples/novelties/break_axe/axe.py
@@ -1,4 +1,22 @@
 from gym_novel_gridworlds2.contrib.polycraft.objects.iron_pickaxe import IronPickaxe
+import pygame, os
+
+# src: https://minecraft.fandom.com/wiki/Axe
+AXE_IMG = pygame.image.load(
+    os.path.join(os.path.dirname(__file__), "Iron_Axe.png")
+)
+AXE_IMG = pygame.transform.scale(AXE_IMG, (20, 20))
+
 
 class Axe(IronPickaxe):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.state = "block"
+    
+    def acted_upon(self, action_name, agent):
+        if action_name == "break":
+            self.state = "floating"
+        return super().acted_upon(action_name, agent)
+
+    def get_img(self):
+        return AXE_IMG
