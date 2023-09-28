4575c003bdbff0fdbdd3b9f3eb563e7952ea7104
waymao <my.waymao+git@gmail.com>
2023-09-28 01:37:19 -0400

    increase limit

--- gym_novel_gridworlds2/envs/__init__.py
+++ gym_novel_gridworlds2/envs/__init__.py
@@ -4,5 +4,5 @@ from gymnasium.envs.registration import register
 register(
     id='NovelGridWorlds-Sequential-v2',
     entry_point='gym_novel_gridworlds2.envs:NovelGridWorldSequentialEnv',
-    max_episode_steps=1000,
+    max_episode_steps=9600,
 )
